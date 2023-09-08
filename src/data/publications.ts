b023edfa889799be3ab613a268af494d2ef5245c
brafumSec <51104856+brafumSec@users.noreply.github.com>
2023-09-08 23:13:16 +0800

    Update publications.ts

--- src/data/publications.ts
+++ src/data/publications.ts
@@ -23,7 +23,7 @@ export default [
       name: "Man-in-the-Middle Attacks without Rogue AP: When WPAs Meet ICMP Redirects  [pdf]",
       authors: "<strong>Feng Xuewei</strong>, Li Qi, Sun Kun, Yang Yuxiang, Xu Ke",
       org: "The 44<sup>th</sup> IEEE Symposium on Security and Privacy (<strong>IEEE S&P</strong>)",
-      link: 'https://tjc-download.weiyun.com/ftn_handler/dc8529697aecf4784d6781ad01da1dc9a5ad8617ce47538b720c2f0a1934d8b1de61ffeca17cf47056385f7b3d4922c89745b67c8929d73532ef141b311e7bc2/Oakland_Feng.pdf?fname=Oakland_Feng.pdf&from=30113&version=3.3.3.3',
+      link: 'https://drive.google.com/u/0/uc?id=1ks4n0PoHeZlJaZdNY74mDA39e7tOML9T&export=download',
        }
   },
   {
@@ -50,7 +50,7 @@ export default [
       name: "D3: Lightweight Secure Fault Localization in Edge Cloud  [pdf]",
       authors: "Fu Songtao, Li Qi, Wang Xiaoliang, Yao Su, <strong>Feng Xuewei</strong>, Wang Ziqiang, Du Xinle, Wan Kao, Xu Ke",
       org: "The 42<sup>nd</sup> International Conference on Distributed Computing Systems (<strong>ICDCS</strong>)",
-      link: 'https://hkc-download.weiyun.com/ftn_handler/5408c735c5f6033d8af5e817583a0b29c0800b50cb686296f29b6539fd22f0ec1ac678091ff568df5986b6ed61e98185cf43442449e7949e3ccff1d84278da76/songtao_icdcs2022.pdf?fname=songtao_icdcs2022.pdf&from=30113&version=3.3.3.3',
+      link: 'https://drive.google.com/u/0/uc?id=1uGQIh9XbTEtLOYA7slXhMHqF96fimz7a&export=download',
     }
   },
   {
@@ -59,7 +59,7 @@ export default [
       name: "Off-Path TCP Hijacking Attacks via the Side Channel of Downgraded IPID  [pdf]",
       authors: "<strong>Feng Xuewei</strong>, Li Qi, Sun Kun, Fu Chuanpu, Xu Ke",
       org: "IEEE/ACM Transactions on Networking (<strong>ToN</strong>)",
-      link: 'https://hkc-download.weiyun.com/ftn_handler/331f1e42604493a30c8ee51e343b09dff70e52d000519f9cb765a4e657bd41066ea7c879b16385abac9bcd6262818ddda828d78e1708c4c7364c1a74cef2f871/Off-Path_TCP_Hijacking_Attacks_via_the_Side_Channel_of_Downgraded_IPID.pdf?fname=Off-Path_TCP_Hijacking_Attacks_via_the_Side_Channel_of_Downgraded_IPID.pdf&from=30113&version=3.3.3.3',
+      link: 'https://drive.google.com/u/0/uc?id=1_e-F6Moh9oNotLbJt7Gf4YUbfOVtczEz&export=download',
     }
   },
   {
@@ -68,7 +68,7 @@ export default [
       name: "Off-path TCP exploits of the mixed IPID assignment  [pdf]",
       authors: "<strong>Feng Xuewei</strong>, Fu Chuanpu, Li Qi, Sun Kun, Xu Ke",
       org: "The 27<sup>th</sup> ACM Conference on Computer and Communications Security (<strong>CCS</strong>)",
-      link: 'https://cqc-download.weiyun.com/ftn_handler/f0c55d3c2bad4e523b2693394e7b7247d956678902f5900414492864a8266adb50442e2b86c0ab2da662694fe6beb4fa06b6f2f99b1c9b36d2fa553c2dbf31aa/Off-Path%20TCP%20Exploits%20of%20the%20Mixed%20IPID%20Assignment.pdf?fname=Off-Path%20TCP%20Exploits%20of%20the%20Mixed%20IPID%20Assignment.pdf&from=30113&version=3.3.3.3',
+      link: 'https://drive.google.com/u/0/uc?id=1OmevwT-GWABcM2q86Ax3BBJN-MGCH7Ke&export=download',
       note: ["Nomination for the Distinguished Paper"]
     }
   },
@@ -78,7 +78,7 @@ export default [
       name: "Enhancing Randomization Entropy of x86-64 Code while Preserving Semantic Consistency  [pdf]",
       authors: "<strong>Feng Xuewei</strong><sup>*</sup>, Wang Dongxia<sup>*</sup>, Lin Zhechao, Kuang Xiaohui, Zhao Gang",
       org: "The 19<sup>th</sup> IEEE International Conference on Trust, Security and Privacy in Computing and Communications (<strong>TrustCom</strong>)",
-      link: 'https://hkc-download.weiyun.com/ftn_handler/3b5c2032e0094e6000ea925093fea811fdc73dd418176837f09a7fd32a3fd5da5ea3a362b5cc67b45df8860b4c23f970f0f720b3a82d5a71af643b5b07465f67/Enhancing_Randomization_Entropy_of_x86-64_Code_while_Preserving_Semantic_Consistency.pdf?fname=Enhancing_Randomization_Entropy_of_x86-64_Code_while_Preserving_Semantic_Consistency.pdf&from=30113&version=3.3.3.3',
+      link: 'https://drive.google.com/u/0/uc?id=1Yvcc7uMA0qyhCRh-vuoh9gpoJMT0jVWd&export=download',
     }
   },
   {
@@ -87,7 +87,7 @@ export default [
       name: "An Approach of Discovering Causal Knowledge for Alert Correlating Based on Data Mining  [pdf]",
       authors: "<strong>Feng Xuewei</strong>, Wang Dongxia, Huang Minhuan, Sun Xiaoxia",
       org: "The 12<sup>th</sup> IEEE Conference on Dependable, Autonomic and Secure Computing (<strong>DASC</strong>)",
-      link: 'https://hkc-download.weiyun.com/ftn_handler/3020a8faa7a57879d4e97e905e1683f3bbfe7c3747be6aa46a595015f48d15d9d1fd50482e991afe60daf8216fabfb4940f6df2e4c92c9027945c7f1af359bbb/An%20Approach%20of%20Discovering%20Causal%20Knowledge%20for%20Alert%20Correlating%20Based%20on%20Data%20Mining.pdf?fname=An%20Approach%20of%20Discovering%20Causal%20Knowledge%20for%20Alert%20Correlating%20Based%20on%20Data%20Mining.pdf&from=30113&version=3.3.3.3',
+      link: 'https://drive.google.com/u/0/uc?id=1_Yw7ecJx4f8ppGaPw3BFOFeaSq-Unr0q&export=download',
     }
   },
   {
@@ -96,7 +96,7 @@ export default [
       name: "Analyzing and Correlating Security Events Using State Machine  [pdf]",
       authors: "<strong>Feng Xuewei</strong>, Wang Dongxia, Zeng Jiemei, Ma Guoqing, Li Jin	",
       org: "The 10<sup>th</sup> IEEE Conference on Computer and Information Technology (<strong>ICCIT</strong>)",
-      link: 'https://hkc-download.weiyun.com/ftn_handler/2034392c267e3afd69fa5eeac6cd0536b917db0e7f0ce21555ff436683ccef1ed39fa2a8af40577ed789a0d3b02a801e8c91444df2caecffaa1bf00d62c1499f/Analyzing%20and%20Correlating%20Security%20Events%20Using%20State%20Machine.pdf?fname=Analyzing%20and%20Correlating%20Security%20Events%20Using%20State%20Machine.pdf&from=30113&version=3.3.3.3',
+      link: 'https://drive.google.com/u/0/uc?id=1CdnMWLzoK2V0BkWmp7isHwy3O2CwZX64&export=download',
     }
   },
 // 其他

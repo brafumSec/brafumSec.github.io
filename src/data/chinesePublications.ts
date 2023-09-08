a6e3bf3514042e4bbb5747308e9ebcd605c9109a
brafumSec <51104856+brafumSec@users.noreply.github.com>
2023-09-08 22:41:09 +0800

    Update chinesePublications.ts

--- src/data/chinesePublications.ts
+++ src/data/chinesePublications.ts
@@ -41,7 +41,7 @@ export default [
       name: '一种基于马尔可夫性质的因果知识挖掘方法  [pdf]',
       authors: '<strong>冯学伟</strong>，王东霞，黄敏桓，李津',
       org: '《计算机研究与发》',
-      link: 'https://share.weiyun.com/SZP57JSl',
+      link: 'https://hkc-download.weiyun.com/ftn_handler/f2d2bcfafe290a8efd99825c948c3608571c836cb1d87e0f9b0b156007f28434f013963474d2d9fca4d0aa6abc7ba81aa55a881ba89ff6c3ef18a3d54544dedd/%E4%B8%80%E7%A7%8D%E5%9F%BA%E4%BA%8E%E9%A9%AC%E5%B0%94%E5%8F%AF%E5%A4%AB%E6%80%A7%E8%B4%A8%E7%9A%84%E5%9B%A0%E6%9E%9C%E7%9F%A5%E8%AF%86%E6%8C%96%E6%8E%98%E6%96%B9%E6%B3%95.pdf?fname=%E4%B8%80%E7%A7%8D%E5%9F%BA%E4%BA%8E%E9%A9%AC%E5%B0%94%E5%8F%AF%E5%A4%AB%E6%80%A7%E8%B4%A8%E7%9A%84%E5%9B%A0%E6%9E%9C%E7%9F%A5%E8%AF%86%E6%8C%96%E6%8E%98%E6%96%B9%E6%B3%95.pdf&from=30113&version=3.3.3.3',
     },
   },
   {
@@ -50,7 +50,7 @@ export default [
       name: '动目标防御技术中的服务多样化机制研究  [pdf]',
       authors: '<strong>冯学伟</strong>，况晓辉，王东霞，李津',
       org: '第十一届中国信息和通信安全大会',
-      link: 'https://share.weiyun.com/lqu7wgjw',
+      link: 'https://tjc-download.weiyun.com/ftn_handler/01c630342b693064ae3b8d1c702850c5634daa1bb059a627ca5f0cfc7eec8654fec6a13cca809aba559592b320dab47a5e86834e58113717fdfc73f7cea487a3/%E5%8A%A8%E7%9B%AE%E6%A0%87%E9%98%B2%E5%BE%A1%E6%8A%80%E6%9C%AF%E4%B8%AD%E7%9A%84%E6%9C%8D%E5%8A%A1%E5%A4%9A%E6%A0%B7%E5%8C%96%E6%9C%BA%E5%88%B6%E7%A0%94%E7%A9%B6.pdf?fname=%E5%8A%A8%E7%9B%AE%E6%A0%87%E9%98%B2%E5%BE%A1%E6%8A%80%E6%9C%AF%E4%B8%AD%E7%9A%84%E6%9C%8D%E5%8A%A1%E5%A4%9A%E6%A0%B7%E5%8C%96%E6%9C%BA%E5%88%B6%E7%A0%94%E7%A9%B6.pdf&from=30113&version=3.3.3.3',
       note: ['大会优秀论文'],
     },
   },

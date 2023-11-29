e60e7825725a4c07c2c03932e5de23bcae586c27
brafumSec <51104856+brafumSec@users.noreply.github.com>
2023-11-29 11:29:43 +0800

    Update intro.tsx

--- src/data/intro.tsx
+++ src/data/intro.tsx
@@ -3,15 +3,15 @@ export default {
   items: [
     {
       key: '1',  // 保证唯一就行
-      label: 'email', // 标签名
-      children: <a href="mailto:fengxw06@126.com">fengxw06@126.com</a>, // 支持 HTML 形式
+      label: 'Email', // 标签名
+      children: <a>fengxw06[at]gmail.com</a>, // 支持 HTML 形式
       span:3, // 包含列的数量，用于控制是否占用单行等
       labelStyle: {}, // 用于控制 label 样式
       contentStyle: {} // 用于控制 children 样式
     },
     {
       key: '2',
-      label: 'research',
+      label: 'Research',
       children: "Network and Information Security",
     },
   ]

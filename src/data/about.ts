aad75a7945f8dae29940137ec701ff37cfd2859a
brafumSec <51104856+brafumSec@users.noreply.github.com>
2023-11-29 09:33:06 +0800

    Update about.ts

--- src/data/about.ts
+++ src/data/about.ts
@@ -8,18 +8,18 @@ export default [
   building blocks, e.g., Wi-Fi, IPv4/IPv6, ICMP, UDP, and TCP. My work has
   received widespread recognition and has been published at leading
   network security conferences, e.g., IEEE S&P, USENIX Security, ACM
-  CCS, and NDSS. I have reported over 30 TCP/IP vulnerabilities,
+  CCS, and NDSS. I have reported over 50 TCP/IP vulnerabilities,
   contributing to CVE/CNVD and working with affected vendors for
   remediation.
   `,
   `
   I have been honored with a number of awards: Frontiers of Science
   Awards by the International Congress of Basic Science (ICBS),
-  Nomination for the Distinguished Paper Award of CCS'2020, and ACM
+  Nomination for the Distinguished Paper Award of CCS'2020, ACM SIGSAC Doctoral Dissertation Award Runner-Up, and ACM
   SIGSAC China Doctoral Dissertation Award. The impact of my research
   has extended to the security community and industry, receiving
   acknowledgments and thanks from well-known organizations and companies
-  like Qualcomm, Wi-Fi Alliance, Linux community, China Telecom, Huawei,
+  like Qualcomm, Wi-Fi Alliance, Linux community, China Telecom, China Unicom, Huawei,
   Alibaba, H3C, Ruijie, and others. Technique communities and media
   outlets such as The Register and APNIC have also reported on my work.
   `,

437920a8e8d136dad7f79f199fab727e55b0d461
brafumSec <51104856+brafumSec@users.noreply.github.com>
2024-09-15 16:23:04 +0800

    Update publications.ts

--- src/data/publications.ts
+++ src/data/publications.ts
@@ -2,7 +2,7 @@ export default [
   {
     date: 2025,
     paper: {
-      name: "An Empirical Study on Remote DoS Attacks against NAT Networks",
+      name: "ReDAN: An Empirical Study on Remote DoS Attacks against NAT Networks",
       authors: "<strong>Feng Xuewei</strong>, Yang Yuxiang, Li Qi, Zhan Xingxiang, Sun Kun, Wang Ziqiang, Wang Ao, Du Ganqiu, Xu Ke",
       org: "The 32<sup>nd</sup> Network and Distributed System Security Symposium (<strong>NDSS</strong>)",
       link: '',
